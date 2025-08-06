8088b15f53510398597bd513dca2a25efd5e6318
Nell Tam <nell@example.com>
2025-08-05 17:01:46 -0700

    Update main.py, test_ml.py, local_api.py, and add CI screenshot

--- local_api.py
+++ local_api.py
@@ -3,15 +3,15 @@ import json
 import requests
 
 # TODO: send a GET using the URL http://127.0.0.1:8000
-r = requests.get("http://127.0.0.1.8000/")
+r = requests.get("http://127.0.0.1:8000/")
 # TODO: print the status code
 print(f"GET status code: {r.status_code}")
 
 # TODO: print the welcome message
 try:
-    print(f"GET response: {r.text}")
+    print("GET response:", r.json())
 except Exception as e:
-    print(f"GET failed to decode JSON:, {e}")
+    print("GET failed to decode JSON:", e)
 
 
 data = {
@@ -19,25 +19,25 @@ data = {
     "workclass": "Private",
     "fnlgt": 178356,
     "education": "HS-grad",
-    "education_num": 10,
-    "marital_status": "Married-civ-spouse",
+    "education-num": 10,
+    "marital-status": "Married-civ-spouse",
     "occupation": "Prof-specialty",
     "relationship": "Husband",
     "race": "White",
     "sex": "Male",
-    "capital_gain": 0,
-    "capital_loss": 0,
-    "hours_per_week": 40,
-    "native_country": "United-States"
+    "capital-gain": 0,
+    "capital-loss": 0,
+    "hours-per-week": 40,
+    "native-country": "United-States"
 }
 
 # TODO: send a POST using the data above
-r = requests.post("http://127.0.0.1.8000/data/", json=data)
+r = requests.post("http://127.0.0.1:8000/data/", json=data)
 
 # TODO: print the status code
 print(f"POST status code: {r.status_code}")
 try:
-    print(f"POST response: {r.text}")
+    print("POST response:", r.json())
 except Exception as e:
-    print(f"POST failed to decode JSON:, {e}")
+    print("POST failed to decode JSON:", e)
 
--- test_ml.py
+++ test_ml.py
@@ -1,28 +1,67 @@
 import pytest
 # TODO: add necessary import
+from ml.model import train_model
+from ml.data import process_data
+import pandas as pd
+from ml.model import compute_model_metrics
+import numpy as np
+from sklearn.model_selection import train_test_split
 
 # TODO: implement the first test. Change the function name and input as needed
-def test_one():
+# load sample data for test
+@pytest.fixture
+def sample_data():
+    
+    df = pd.read_csv("data/census.csv")
+    
+    cat_features = [
+        "workclass", "education", "marital-status", "occupation",
+        "relationship", "race", "sex", "native-country"
+    ]
+    train, _ = train_test_split(df, test_size=0.20)
+    X_train, y_train, encoder, lb = process_data (
+        train, categorical_features=cat_features, label="salary", training=True
+    )
+    return X_train, y_train
+
+def test_train_model_returns_classifier(sample_data):
     """
-    # add description for the first test
+    Test that train model retunrns a trained classifier object
     """
-    # Your code here
+    X_train, y_train = sample_data
+    model = train_model(X_train, y_train)
+    from sklearn.base import ClassifierMixin
+    assert isinstance(model, ClassifierMixin)
     pass
 
 
 # TODO: implement the second test. Change the function name and input as needed
-def test_two():
+def test_compute_model_metrics_output():
     """
-    # add description for the second test
+    Test that compute_model_metrics t=returns float precision, recall, f1
     """
-    # Your code here
+    y = np.array([1, 0, 1, 0])
+    preds = np.array([1, 0, 1, 1])
+    precision, recall, fbeta = compute_model_metrics(y, preds)
+    assert isinstance(precision, float)
+    assert isinstance(recall, float)
+    assert isinstance(fbeta, float)
+        
     pass
 
 
 # TODO: implement the third test. Change the function name and input as needed
-def test_three():
+def test_process_data_shape():
     """
-    # add description for the third test
+    Test that process_data returns arrayswith matching number of rows
     """
-    # Your code here
+    df = pd.read_csv("data/census.csv")
+    
+    cat_features = [
+        "workclass", "education", "marital-status", "occupation",
+        "relationship", "race", "sex", "native-country"
+    ]
+    train, _ = train_test_split(df, test_size=0.20)
+    x, y, _, _ = process_data(train, categorical_features=cat_features, label="salary", training=True)
+    assert len(x) == len(y)
     pass
